57431510025852a38442c95cc52348b20f6e141c
thesoundfromthesky <3rdworldstrawberry@gmail.com>
2021-10-18 16:21:55 -0500

    Build a bottom navigation component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,45 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import { useHistory } from 'react-router-dom';
-import styled from 'styled-components';
+import styled from "styled-components";
+
+import BottomNav from "./Bottom-Nav";
 
 const Landing = () => {
+  const Header = styled.h1`
+    font-family: "Grand Hotel", cursive, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 56px;
+    line-height: 42px;
+    color: black;
+    /* color: #FFFFFF */
+  `;
+  const Slogan = styled.div`
+    font-family: "Cabin";
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 24px;
+  `;
 
-    const Header = styled.h1`
-        font-family: 'Grand Hotel', cursive, sans-serif;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 56px;
-        line-height: 42px;
-        color: black;
-        /* color: #FFFFFF */
-    `
-    const Slogan = styled.div`
-        font-family: 'Cabin';
-        font-style: normal;
-        font-weight: normal;
-        font-size: 20px;
-        line-height: 24px;
-    `
+  return (
+    // all of our stuff right here
+    // logo
+    // app name
+    <>
+      <Header>Apartmates</Header>
+      <Slogan>Find your place and the people to share it with</Slogan>
+      <BottomNav />
+    </>
+    // catchphrase
 
-    
-
-    return (
-
-        // all of our stuff right here
-        // logo
-        // app name
-        <>
-            <Header>Apartmates</Header>
-            <Slogan>Find your place and the people to share it with</Slogan>
-        </>
-        // catchphrase
-
-        // button
-        // button
-        // button
-        
-    )
-
-}
+    // button
+    // button
+    // button
+  );
+};
 
 export default Landing;
