d39fecc680e7a5955618adc46b8dced4f36db563
ORiveraJr84 <oriverajr84@gmail.com>
2021-10-19 17:50:01 -0400

    added SignupListings component to main ap screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import Landing from './components/Landing';
-import TenantHome from './components/TenantHome';
-import ProfileCreation from './components/ProfileCreation'; 
+import Landing from "./components/Landing";
+import TenantHome from "./components/TenantHome";
+import ProfileCreation from "./components/ProfileCreation";
+import SignupListings from "./components/SignUp-Listings";
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
-      <TenantHome/>
+      <Landing />
+      <TenantHome />
       <ProfileCreation />
+      <SignupListings />
     </div>
   );
 }
