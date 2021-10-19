77e47fb394184a5d638f67a170aa8d6cad0ebf4d
Charlieu Lu <charlie.m.lu1@gmail.com>
2021-10-19 12:41:52 -0700

    some fake data for homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import Landing from './components/Landing';
-import TenantHome from './components/TenantHome';
+import TenantHome from './components/Homepage';
 import ProfileCreation from './components/ProfileCreation'; 
 
 function App() {
