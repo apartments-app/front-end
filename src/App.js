18aa847670002ce20515b1d180a3eeb65dd51141
ORiveraJr84 <oriverajr84@gmail.com>
2021-10-20 16:48:55 -0400

    added signIn component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Landing from "./components/Landing";
 import TenantHome from "./components/Homepage";
 import ProfileCreation from "./components/ProfileCreation";
 import SignUpListings from "./components/SignUp-Listings";
-
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
     <div className="App">
-
       {/* test the landing page */}
       <Landing />
       {/* test the tenant home */}
@@ -17,7 +16,7 @@ function App() {
       {/* test the profile creation */}
       <ProfileCreation />
       <SignUpListings />
-
+      <SignIn />
     </div>
   );
 }
